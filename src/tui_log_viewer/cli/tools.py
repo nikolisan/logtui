75dd72ef9068a654f4226b56b9aa2a90b31d3c57
Nick Andreakos <18707825+nikolisan@users.noreply.github.com>
2026-07-25 17:38:31 +0100

    Added supported for coloured output in the terminal cli. Added stubs for coloured class

--- src/tui_log_viewer/cli/tools.py
+++ src/tui_log_viewer/cli/tools.py
@@ -1,23 +1,32 @@
 import textwrap
 
 from tui_log_viewer.cli.mappers import LogEntry
+from tui_log_viewer.utils.colour import Coloured
 
 
-def printer_header(directory: str):
+def printer_header(directory: str) -> None:
     print(f"logtui: Follow log entries at {directory}\n")
-    print(f"{'Timestamp':19} | {'Module':50} | {'Level':7} | {'Message':50}")
-    print(f"{'-' * 19} + {'-' * 50} + {'-' * 7} + {'-' * 50}")
+    print(f"{'Timestamp':20} | {'Module':70} | {'Level':10} | {'Message':70}")
+    print(f"{'-' * 20} + {'-' * 70} + {'-' * 10} + {'-' * 70}")
 
 
-def printer(entry: LogEntry):
+def printer(entry: LogEntry) -> None:
     timestamp = entry.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-    module = entry.module
-    level = entry.level
-    message = entry.message
 
-    print(
-        f"{timestamp:19} | "
-        + f"{textwrap.shorten(module, 50):<50} | "
-        + f"{textwrap.shorten(level, 7):<7} | "
-        + f"{textwrap.shorten(message, 50):<50}"
+    module = textwrap.shorten(entry.module, width=70, placeholder="...")
+    level = textwrap.shorten(entry.level, width=10, placeholder="...")
+    message = textwrap.shorten(entry.message, width=70, placeholder="...")
+
+    timestamp_field = f"{timestamp:<20}"
+    module_field = f"{module:<70}"
+    level_field = f"{level:<10}"
+    message_field = f"{message:<70}"
+
+    module_field = Coloured.BLUE(module_field)
+    level_field = Coloured.for_level(
+        level_field,
+        entry.level,
+        bold=True,
     )
+
+    print(f"{timestamp_field} | {module_field} | {level_field} | {message_field}")
--- src/tui_log_viewer/runners/cli.py
+++ src/tui_log_viewer/runners/cli.py
@@ -33,8 +33,8 @@ def main():
             asyncio.run(start_cli_no_tui(args))
     except KeyboardInterrupt:
         logger.info("Stopped following log file")
-    except Exception:
-        logger.exception("Unhandled exception in CLI.")
+    except Exception as e:
+        logger.exception(f"{type(e).__name__}: {e}")  # noqa: TRY401
 
 
 if __name__ == "__main__":
