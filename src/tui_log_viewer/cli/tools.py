8b54d4db4e2c51bcb4e0144add1d5ac58563f2b1
Nick Andreakos <18707825+nikolisan@users.noreply.github.com>
2026-07-25 15:51:35 +0100

    Added pyright and pyright pre-commit hooks, updated github actions

---
+++ src/tui_log_viewer/cli/tools.py
@@ -0,0 +1,23 @@
+import textwrap
+
+from tui_log_viewer.cli.mappers import LogEntry
+
+
+def printer_header(directory: str):
+    print(f"logtui: Follow log entries at {directory}\n")
+    print(f"{'Timestamp':19} | {'Module':50} | {'Level':7} | {'Message':50}")
+    print(f"{'-' * 19} + {'-' * 50} + {'-' * 7} + {'-' * 50}")
+
+
+def printer(entry: LogEntry):
+    timestamp = entry.timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    module = entry.module
+    level = entry.level
+    message = entry.message
+
+    print(
+        f"{timestamp:19} | "
+        f"{textwrap.shorten(module, 50):<50} | "
+        f"{textwrap.shorten(level, 7):<7} | "
+        f"{textwrap.shorten(message, 50):<50}"
+    )
--- src/tui_log_viewer/runners/cli.py
+++ src/tui_log_viewer/runners/cli.py
@@ -1,32 +1,11 @@
 import asyncio
 import logging
-import textwrap
 
 from tui_log_viewer.cli import LogParser
-from tui_log_viewer.cli.mappers import LogEntry
+from tui_log_viewer.cli.tools import printer_header, printer
 from tui_log_viewer.runners.arg_parser import parse_arguments
 
 logger = logging.getLogger(__name__)
-
-
-def printer_header(directory: str):
-    print(f"logtui: Follow log entries at {directory}\n")
-    print(f"{'Timestamp':19} | {'Module':50} | {'Level':7} | {'Message':50}")
-    print(f"{'-' * 19} + {'-' * 50} + {'-' * 7} + {'-' * 50}")
-
-
-def printer(entry: LogEntry):
-    timestamp = entry.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-    module = entry.module
-    level = entry.level
-    message = entry.message
-
-    print(
-        f"{timestamp:19} | "
-        f"{textwrap.shorten(module, 50):<50} | "
-        f"{textwrap.shorten(level, 7):<7} | "
-        f"{textwrap.shorten(message, 50):<50}"
-    )
 
 
 async def start_cli_no_tui(args):
